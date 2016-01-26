28698895a27a9eb03ccaccd33c98aaf549197681
Rashad <mohammedrashadkm@gmail.com>
2016-01-26 23:57:32 +0100

    start search packages in system first

--- cmake/SuperFind.cmake
+++ cmake/SuperFind.cmake
@@ -64,124 +64,138 @@ function(super_find_package name)
   set(_WITH_EXTERNAL_OPTION_DEFAULT TRUE)
   
   option(WITH_${name} "Set ON to use ${name}" ${_WITH_OPTION_DEFAULT})
-  option(WITH_EXTERNAL_${name} "Set ON to use ${name} from custom install location" ${_WITH_EXTERNAL_OPTION_DEFAULT})
+#  option(WITH_INTERNAL_${name} "Set ON to download and configure ${name}. ON by default" ${_WITH_EXTERNAL_OPTION_DEFAULT})
   
   if(${WITH_${name}})
+    option(WITH_${name}_EXTERNAL "Set ON to use external ${name}" OFF)
+    if(WITH_${name}_EXTERNAL)
+      
+      set(PKG_NAME ${name})
+      string(TOUPPER ${name} PKG_NAME_)
     
-    set(PKG_NAME ${name})
-    
-    simple_message("WITH_${name} is set. Searching for ${PKG_NAME}")
-    
-    #find the repo name.
-    if(super_find_package_REPOSITORY_NAME)
-      set(PKG_REPOSITORY_NAME ${super_find_package_REPOSITORY_NAME})
-    elseif(${PKG_NAME}_REPOSITORY_NAME)
-      set(PKG_REPOSITORY_NAME ${${PKG_NAME}_REPOSITORY_NAME})
-    else()
-      set(PKG_REPOSITORY_NAME ${PKG_NAME})
-    endif()
-    
-    if(EXISTS ${ep_base}/Build/${PKG_NAME}/${PKG_NAME}Config.cmake)
-      find_package(${PKG_NAME} PATHS ${ep_base}/Build/${PKG_NAME})
-      if(${PKG_NAME}_FOUND)
-	simple_message("Found ${PKG_NAME} from ${ep_base}/Build/${PKG_NAME}/${PKG_NAME}Config.cmake")
-	add_custom_target(${PKG_NAME}
-	  COMMAND ${CMAKE_COMMAND} --build "${ep_base}/Build/${PKG_NAME}"
-	  WORKING_DIRECTORY "${ep_base}/Build/${PKG_NAME}")
-      endif()
-    else()
-      if(NOT ${WITH_EXTERNAL_${name}})
+      simple_message("WITH_${name} is set. Searching for ${PKG_NAME}")
+      if(NOT super_find_package_COMPONENTS)
 	find_package(${PKG_NAME} QUIET)
       else()
-	find_package(${PKG_NAME} REQUIRED)
+	find_package(${PKG_NAME} COMPONENTS ${super_find_package_COMPONENTS} QUIET)
       endif()
-      if(${PKG_NAME}_FOUND)
+      if(${PKG_NAME_}_FOUND)
         simple_message("Found ${PKG_NAME} from find_package(${PKG_NAME})")
-	if(NOT ${WITH_EXTERNAL_${name}})
-	  #do an empty target.
-	  add_custom_target(${PKG_NAME})
-	endif()  
-      endif()
-    endif()
-    
-    if(NOT ${WITH_EXTERNAL_${name}})
+	# do an empty target.
+	add_custom_target(${PKG_NAME})
+
+      else() # if(${PKG_NAME}_FOUND) find_package
+	if(EXISTS ${ep_base}/Build/${PKG_NAME}/${PKG_NAME}Config.cmake)
+
+	  if(NOT super_find_package_COMPONENTS)
+	    find_package(${PKG_NAME} PATHS ${ep_base}/Build/${PKG_NAME})
+	  else()
+	    find_package(${PKG_NAME} COMPONENTS ${super_find_package_COMPONENTS} PATHS ${ep_base}/Build/${PKG_NAME})
+	  endif()
+	  
+
+	  if(${PKG_NAME_}_FOUND)
+	    simple_message("Found ${PKG_NAME} from ${ep_base}/Build/${PKG_NAME}/${PKG_NAME}Config.cmake")
+	    set( ${PKG_NAME_}_FOUND_FROM_CONFIG TRUE CACHE "" INTERNAL)
+	    add_custom_target(${PKG_NAME}
+	      COMMAND ${CMAKE_COMMAND} --build "${ep_base}/Build/${PKG_NAME}"
+	      WORKING_DIRECTORY "${ep_base}/Build/${PKG_NAME}")
+	  endif()
+	else() #if(EXISTS ${ep_base}/Build/${PKG_NAME}/${PKG_NAME}Config.cmake)
+	  
+	  if(NOT DEFINED GIT_EXECUTABLE)
+	    find_package(Git)
+	  endif()
+	  
+	  #find the repo name.
+	  if(super_find_package_REPOSITORY_NAME)
+	    set(PKG_REPOSITORY_NAME ${super_find_package_REPOSITORY_NAME})
+	  elseif(${PKG_NAME}_REPOSITORY_NAME)
+	    set(PKG_REPOSITORY_NAME ${${PKG_NAME}_REPOSITORY_NAME})
+	  else()
+	    set(PKG_REPOSITORY_NAME ${PKG_NAME})
+	  endif()
+	  
+	  simple_message("Adding ExternalProject ${PKG_NAME}")
+	  
+	  ExternalProject_Add(${PKG_NAME}
+	    GIT_REPOSITORY ${EP_URL}/${PKG_REPOSITORY_NAME}
+	    DOWNLOAD_COMMAND ""
+	    CONFIGURE_COMMAND ""
+	    UPDATE_COMMAND ""        
+	    INSTALL_COMMAND)
+	  
+	  set(${PKG_NAME}_INSTALL_DIR ${ep_base}/Install/${PKG_NAME})
+	  
+	  #download source code.
+	  if(NOT EXISTS "${ep_base}/Stamp/${PKG_NAME}/${PKG_NAME}-download")
+	    simple_message("Downloading sources via git clone ${EP_URL}/${PKG_REPOSITORY_NAME}")
+	    execute_process(COMMAND ${GIT_EXECUTABLE} clone --depth 1 ${EP_URL}/${PKG_REPOSITORY_NAME} ${PKG_NAME}
+	      WORKING_DIRECTORY  ${ep_base}/Source RESULT_VARIABLE clone_rv)
+	    if(NOT clone_rv)
+	      execute_process(COMMAND ${CMAKE_COMMAND}  -E touch "${PKG_NAME}-download"
+		WORKING_DIRECTORY ${ep_base}/Stamp/${PKG_NAME} )
+	    else()
+              message(FATAL_ERROR "error cloning sources")
+              return()
+	    endif()
+	  endif() #if(NOT EXISTS "${ep_base}/Stamp/${PKG_NAME}/${PKG_NAME}-download")
+	  
+	endif() #if(EXISTS ${ep_base}/Build/${PKG_NAME}/${PKG_NAME}Config.cmake)
+      endif() # if(${PKG_NAME}_FOUND) find_package
       
-    if(NOT ${PKG_NAME}_FOUND)
+      if(${PKG_NAME_}_FOUND AND ${PKG_NAME_}_FOUND_FROM_CONFIG)
+	get_last_commit_id(${PKG_NAME} commit_id_before_pull)
+	simple_message("updating sources. HEAD is now at: ${commit_id_before_pull}")
 	
-      if(NOT DEFINED GIT_EXECUTABLE)
-	find_package(Git)
-      endif()
-      
-      simple_message("Adding ExternalProject ${PKG_NAME}")
-      
-      ExternalProject_Add(${PKG_NAME}
-	GIT_REPOSITORY ${EP_URL}/${PKG_REPOSITORY_NAME}
-	DOWNLOAD_COMMAND ""
-	CONFIGURE_COMMAND ""
-	UPDATE_COMMAND ""        
-	INSTALL_COMMAND)
-      
-      set(${PKG_NAME}_INSTALL_DIR ${ep_base}/Install/${PKG_NAME})
-      
-      #download source code.
-      if(NOT EXISTS "${ep_base}/Stamp/${PKG_NAME}/${PKG_NAME}-download")
-	simple_message("Downloading sources via git clone ${EP_URL}/${PKG_REPOSITORY_NAME}")
-	execute_process(COMMAND ${GIT_EXECUTABLE} clone --depth 1 ${EP_URL}/${PKG_REPOSITORY_NAME} ${PKG_NAME}
-	  WORKING_DIRECTORY  ${ep_base}/Source RESULT_VARIABLE clone_rv)
-	if(NOT clone_rv)
-	  execute_process(COMMAND ${CMAKE_COMMAND}  -E touch "${PKG_NAME}-download"
-            WORKING_DIRECTORY ${ep_base}/Stamp/${PKG_NAME} )
-	else()
-          message(FATAL_ERROR "error cloning sources")
-          return()
+	execute_process(COMMAND ${GIT_EXECUTABLE} pull
+	  WORKING_DIRECTORY ${ep_base}/Source/${PKG_NAME}
+	  OUTPUT_VARIABLE pull_ov)
+	
+	get_last_commit_id(${PKG_NAME} commit_id_after_pull)
+	if(NOT "${commit_id_after_pull}" STREQUAL "${commit_id_before_pull}")
+	  simple_message("Sources changed. HEAD is now at: ${commit_id_after_pull}")
+	  execute_process(COMMAND
+	    ${CMAKE_COMMAND} -E remove_directory "${ep_base}/Build/${PKG_NAME}"
+	    WORKING_DIRECTORY ${ep_base}/Build/${PKG_NAME})   
 	endif()
-      endif() #if(NOT EXISTS "${ep_base}/Stamp/${PKG_NAME}/${PKG_NAME}-download")
-    endif()     # if(NOT ${PKG_NAME}_FOUND)
-    
-    if(NOT ${PKG_NAME}_FOUND OR EXISTS "${ep_base}/Stamp/${PKG_NAME}/${PKG_NAME}-download")
-      get_last_commit_id(${PKG_NAME} commit_id_before_pull)
-      simple_message("updating sources. HEAD is now at: ${commit_id_before_pull}")
-      
-      execute_process(COMMAND ${GIT_EXECUTABLE} pull
-	WORKING_DIRECTORY ${ep_base}/Source/${PKG_NAME}
-	OUTPUT_VARIABLE pull_ov)
-      
-      get_last_commit_id(${PKG_NAME} commit_id_after_pull)
-      if(NOT "${commit_id_after_pull}" STREQUAL "${commit_id_before_pull}")
-	simple_message("Sources changed. HEAD is now at: ${commit_id_after_pull}")
-	execute_process(COMMAND
-	  ${CMAKE_COMMAND} -E remove_directory "${ep_base}/Build/${PKG_NAME}"
-	  WORKING_DIRECTORY ${ep_base}/Build/${PKG_NAME})   
-      endif()
-      
-      #configure
-      if(NOT super_find_package_CMAKE_ARGS)
-	set(super_find_package_CMAKE_ARGS "-G${CMAKE_GENERATOR}")
+	
+	#configure
+	if(NOT super_find_package_CMAKE_ARGS)
+	  set(super_find_package_CMAKE_ARGS "-G${CMAKE_GENERATOR}")
+	else()
+	  set(super_find_package_CMAKE_ARGS "${super_find_package_CMAKE_ARGS} -G${CMAKE_GENERATOR}")
+	endif()
+	simple_message("Running cmake configure on ${ep_base}/Source/${PKG_NAME}")    
+	execute_process(COMMAND ${CMAKE_COMMAND} ${ep_base}/Source/${PKG_NAME}
+	  "-DCMAKE_INSTALL_PREFIX=${${PKG_NAME}_INSTALL_DIR}"
+	  "-DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}"
+	  "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
+	  "${super_find_package_CMAKE_ARGS}"
+	  WORKING_DIRECTORY ${ep_base}/Build/${PKG_NAME}
+	  RESULT_VARIABLE configure_rv)
+	
+	if(NOT configure_rv)
+	  simple_message("Loading config file from ${ep_base}/Build/${PKG_NAME}/${PKG_NAME}Config.cmake")
+	  include(${ep_base}/Build/${PKG_NAME}/${PKG_NAME}Config.cmake)
+	else()
+	  message(FATAL_ERROR "error running cmake configure on ${PKG_NAME}")
+	endif()
+	
+      endif()  #if(NOT ${PKG_NAME}_FOUND OR EXISTS "${ep_base}/Stamp/${PKG_NAME}/${PKG_NAME}-download")
+      endif() # 	if(${WITH_EXTERNAL_${name}})
+    else()
+      if(NOT super_find_package_COMPONENTS)
+	find_package(${name} REQUIRED)
       else()
-	set(super_find_package_CMAKE_ARGS "${super_find_package_CMAKE_ARGS} -G${CMAKE_GENERATOR}")
+	find_package(${name} COMPONENTS ${super_find_package_COMPONENTS} REQUIRED)
       endif()
-      simple_message("Running cmake configure on ${ep_base}/Source/${PKG_NAME}")    
-      execute_process(COMMAND ${CMAKE_COMMAND} ${ep_base}/Source/${PKG_NAME}
-	"-DCMAKE_INSTALL_PREFIX=${${PKG_NAME}_INSTALL_DIR}"
-	"-DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}"
-	"-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
-	"${super_find_package_CMAKE_ARGS}"
-	WORKING_DIRECTORY ${ep_base}/Build/${PKG_NAME}
-	RESULT_VARIABLE configure_rv)
-      
-      if(NOT configure_rv)
-	simple_message("Loading config file from ${ep_base}/Build/${PKG_NAME}/${PKG_NAME}Config.cmake")
-	include(${ep_base}/Build/${PKG_NAME}/${PKG_NAME}Config.cmake)
-      else()
-	message(FATAL_ERROR "error running cmake configure on ${PKG_NAME}")
-      endif()
-      
-    endif()  #if(NOT ${PKG_NAME}_FOUND OR EXISTS "${ep_base}/Stamp/${PKG_NAME}/${PKG_NAME}-download")
+    endif()     # if(WITH_${name})
 
-  endif() # 	if(NOT ${WITH_EXTERNAL_${name}})
-  endif()     # if(WITH_${name})
-endfunction()
-
+    string(TOUPPER ${name} name_)
+    set(TARGET_LINK_LIBS ${TARGET_LINK_LIBS} ${${name_}_LIBRARIES} PARENT_SCOPE)
+  endfunction()
+  
 
 macro(simple_message msg)
   if(NOT SEARCH_PACKAGE_QUIETLY)
